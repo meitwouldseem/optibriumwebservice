a5463cee65eab071cc1a4e4202095d998029480d
Matthew Burren <mburren@optibrium.com>
2024-12-16 11:24:06 +0000

    Move routes into directory.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use redis::{aio::MultiplexedConnection, RedisResult};
 use actix_web::{web, App, HttpServer};
 
-mod health_check;
-mod add_user;
-mod check_password;
-mod get_usernames;
+mod routes;
 mod username_and_password;
 
 async fn make_redis_connection(address: &str) -> RedisResult<MultiplexedConnection>
@@ -21,10 +18,10 @@ async fn main() {
 
     HttpServer::new(move || {
         App::new()
-            .route("health_check", web::get().to(health_check::health_check))
-            .route("get_usernames", web::get().to(get_usernames::get_usernames))
-            .route("add_user", web::post().to(add_user::add_user))
-            .route("check_password", web::post().to(check_password::check_password))
+            .route("health_check", web::get().to(routes::health_check::health_check))
+            .route("get_usernames", web::get().to(routes::get_usernames::get_usernames))
+            .route("add_user", web::post().to(routes::add_user::add_user))
+            .route("check_password", web::post().to(routes::check_password::check_password))
             .app_data(web::Data::new(redis_connection.clone()))
     })
     .bind("127.0.0.1:8000").expect("Could not bind server to address")
---
+++ src/routes/mod.rs
@@ -0,0 +1,4 @@
+pub mod add_user;
+pub mod check_password;
+pub mod get_usernames;
+pub mod health_check;
